b54b1e3139ebd8fc5b845ed28493140a1ca38290
OmarHamada1996 <omer.jasser@gmail.com>
2023-05-24 19:17:21 +0000

    class18

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,6 +1,5 @@
 
 import {Card} from 'react-bootstrap'
-// import {Button} from 'react-bootstrap'
 import Button from 'react-bootstrap/Button'
 import ModalMovie from '../ModalMovie/ModalMovie'
 import { useState } from 'react'
